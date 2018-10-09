761d7a949467449be84fb41e18173e4d6b517356
mwcvitkovic <mwcvitkovic@gmail.com>
2018-10-09 09:55:22 -0700

    First commit

---
+++ data/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+import os
+
+from .Tasks import *
+
+project_root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
